b4d2934ec60ba98ca41d6e4ab3c37a591b53ecad
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-03-22 22:11:53 -0600

    intentando login 2.2

--- src/auth.js
+++ src/auth.js
@@ -1,41 +1,41 @@
-// export const authentication = async ({ email, password }) => { //mi componente tiene que esperar a que mi servicio se ejecute por completo para que el token no me lo devuelva en blanco
-    // let token = null;
-    // await fetch('http://localhost:8080/login', {
-    //   method: 'POST',
-    //   headers: {
-        // "Content-Type": "application/json", 
-//   
-        // "Authorization": "Bearer (token)"    
-    //   },
-    //   
-    //   body: JSON.stringify({ email, password }) //JSON a texto plano
-    // })
-    //   .then(res => {
-        // token = {
-        //   ...token,
-        //   ok: false,
-        //   status: res.status
-        // }
-        // return res.json()
-    //   })
-    //   .then(res => {
-//   
-        // console.log('res', res)
-        // token = {
-        //   ok: (token.status == 200) ? true : false, //respuesta correcta
-        //   message: (token.status == 200) ? "Login ejecutado de forma correcta" : res,
-        //   token: (token.status == 200) ? res : null
-        // };
-    //   })
-    //   .catch(error => {
-        // console.log(error, error.response)
-        // token = {
-        //   ok: false,
-        //   message: "Error de logueo",
-        //   token: null
-//   
-        // }
-    //   });
-    // return token;
-//   
-//   }
+export const authentication = async ({ email, password }) => { //mi componente tiene que esperar a que mi servicio se ejecute por completo para que el token no me lo devuelva en blanco
+    let token = null;
+    await fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json", 
+  
+        "Authorization": "Bearer (token)"    
+      },
+      
+      body: JSON.stringify({ email, password }) //JSON a texto plano
+    })
+      .then(res => {
+        token = {
+          ...token,
+          ok: false,
+          status: res.status
+        }
+        return res.json()
+      })
+      .then(res => {
+  
+        console.log('res', res)
+        token = {
+          ok: (token.status == 200) ? true : false, //respuesta correcta
+          message: (token.status == 200) ? "Login ejecutado de forma correcta" : res,
+          token: (token.status == 200) ? res : null
+        };
+      })
+      .catch(error => {
+        console.log(error, error.response)
+        token = {
+          ok: false,
+          message: "Error de logueo",
+          token: null
+  
+        }
+      });
+    return token;
+  
+  }
