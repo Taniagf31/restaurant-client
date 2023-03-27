3aad8adee02bf647990e208ee8f1c9ec266fca19
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-03-26 19:21:22 -0600

    comenzando Products css

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,13 +2,23 @@ import "./css-components/Products.css"
 
 export const Products = () => {
   return (
-    <h1>Products</h1>
-  )
-}
+    <div className="container-nav">
+    <div className="container-logo">
+      <img className="img-nav" src="./logo.png" alt="logo-nav" />
+      <img className="user" src="./woman.png" alt="user" />
+      <h3>Susana Admin</h3>
+    </div>
+    <div className="barra-nav">
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Orders</li>
+        <li>Users</li>
+      </ul>
+    </div></div>
+  );
+};
 
-
-
-// 
 
 
 
